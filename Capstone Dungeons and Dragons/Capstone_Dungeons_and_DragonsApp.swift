8234bcb4b4d4850236754354b5c61ad9c21994f6
Samantha Hunt <ariaandsam@gmail.com>
2023-01-28 17:59:42 -0800

    Initial Commit

---
+++ Capstone Dungeons and Dragons/Capstone_Dungeons_and_DragonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Capstone_Dungeons_and_DragonsApp.swift
+//  Capstone Dungeons and Dragons
+//
+//  Created by Samantha Hunt on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Capstone_Dungeons_and_DragonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
